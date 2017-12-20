15ad3b7a691e1ea6447472daa8de345573c567cd
Julian Muratori <jmuratori@ujafed.org>
2017-12-20 13:48:24 -0500

    making my first component

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ajax } from 'jquery';
 
 class App extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        gameNumber: {
+          homeTeam: "",
+          visitingTeam: "",
+          sagarinSpread: "",
+          fiveThirtyEightSpread: ""
+        }
+      }
+    }
     render() {
       return (
         <div>
-          Hello
+
+
+          <div className="pointSpread">
+            <input type="text" placeholder="Game No." className="Game-Number"/>
+            <input type="text" placeholder="Home Team"/>
+            <input type="text" placeholder="Visiting Team"/>
+            <input type="text" placeholder="Sagarin Spread"/>
+            <input type="text" placeholder="538 Spread"/>
+            <button>Complete Entry</button>
+          </div>
+
+          
         </div>
       )
     }
