4067e6d3860552bc7bb0c8c2b61c6c2f36f3f749
Julian Muratori <jmuratori@ujafed.org>
2017-12-21 14:31:23 -0500

    setting some initial state

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -6,26 +6,51 @@ class App extends React.Component {
     constructor() {
       super();
       this.state = {
-        gameNumber: {
-          homeTeam: "",
-          visitingTeam: "",
-          sagarinSpread: "",
-          fiveThirtyEightSpread: ""
-        }
+        games: []
       }
+    }
+
+    addGame(e) {
+      let games = this.state.games;
+      let game = {};
+      
+      
+      // game.gameNumber = this.gameNumber.value;
+      game.gameNumber = this.gameNumber.value;
+      game.homeTeam = this.homeTeam.value;
+      game.visitingTeam = this.visitingTeam.value;
+      game.sagarinSpread = this.sagarinSpread.value;
+      game.fiveThirtyEightSpread = this.fiveThirtyEightSpread.value;
+
+      games.push(game);
+
+      // grabs text from form
+      e.preventDefault();
+
+      // adds game info to state
+      this.setState({games})
+
+      
     }
     render() {
       return (
         <div>
 
 
-          <div className="pointSpread">
-            <input type="text" placeholder="Game No." className="Game-Number"/>
-            <input type="text" placeholder="Home Team"/>
-            <input type="text" placeholder="Visiting Team"/>
-            <input type="text" placeholder="Sagarin Spread"/>
-            <input type="text" placeholder="538 Spread"/>
-            <button>Complete Entry</button>
+          <div>
+            <form action="" className="pointSpread" onSubmit={(e) => this.addGame(e)}>
+              <input type="text" placeholder="Game No." className="Game-Number" 
+                ref={(input) => { this.gameNumber = input }}/>
+              <input type="text" placeholder="Home Team" 
+                ref={(input) => { this.homeTeam = input }}/>
+              <input type="text" placeholder="Visiting Team" 
+                ref={(input) => { this.visitingTeam = input }}/>
+              <input type="text" placeholder="Sagarin Spread" 
+                ref={(input) => { this.sagarinSpread = input }}/>
+              <input type="text" placeholder="538 Spread" 
+                ref={(input) => { this.fiveThirtyEightSpread = input }}/>
+              <button>Complete Entry</button>
+            </form>
           </div>
 
           
